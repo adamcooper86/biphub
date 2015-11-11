8dd96fdbb9dce6cbbe67279fb3a851d0c87bcf44
Adam Fluke <fluke.adam@gmail.com>
2015-11-11 08:57:43 -0800

    Create migration fro school resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110032811) do
+ActiveRecord::Schema.define(version: 20151111162119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
