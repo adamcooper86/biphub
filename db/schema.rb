3176bc4e4537388ab1799a434b19292d7f44243a
Adam Fluke <fluke.adam@gmail.com>
2015-11-17 15:04:45 -0800

    Fix unresolved mrege conflict in users/show

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema.define(version: 20151117211428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cards", force: :cascade do |t|
     t.integer  "student_id"
     t.integer  "teacher_id"
