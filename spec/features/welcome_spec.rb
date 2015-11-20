55758734a46f8f7afc2d6f2a6153e10ab260150e
Adam Fluke <fluke.adam@gmail.com>
2015-11-20 00:08:44 -0800

    Update homepage senario to test for login link

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -4,5 +4,6 @@ feature "Going to the homepage", js: true do
   scenario "Visiting the homepage" do
     visit root_path
     expect(page).to have_content 'BipHub'
+    expect(page).to have_content 'Login'
   end
 end
