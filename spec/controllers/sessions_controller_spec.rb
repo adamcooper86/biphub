e1c7e1c6d070e6b4b66bfeef74c5fa7bf4d87648
Adam Fluke <fluke.adam@gmail.com>
2015-11-22 23:31:45 -0800

    Add tests for revised session#create invalid attempts

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -15,5 +15,29 @@ RSpec.describe Api::V1::SessionsController, :type => :controller do
       expect(JSON.parse(response.body)["token"]).to be_truthy
       expect(JSON.parse(response.body)["token"]).to be_a String
     end
+
+    it "has a 403 status code for invalid email" do
+      xhr :post, :create, { email: "wrongemail@gmail.com", password: user.password }
+      expect(response).not_to be_success
+      expect(response).to have_http_status(403)
+    end
+
+    it "returns a text message that email is invalid" do
+      xhr :post, :create, { email: "wrongemail@gmail.com", password: user.password }
+      expect(response.body).to be_a String
+      expect(response.body).to eq("User With That Email Not Found")
+    end
+
+    it "has a 403 status code for invalid password" do
+      xhr :post, :create, { email: user.email, password: "wrongpassword" }
+      expect(response).not_to be_success
+      expect(response).to have_http_status(403)
+    end
+
+    it "returns a text message that email is invalid" do
+      xhr :post, :create, { email: user.email, password: "wrongpassword" }
+      expect(response.body).to be_a String
+      expect(response.body).to eq("User and Password Don't Match")
+    end
   end
 end
