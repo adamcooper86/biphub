1fccf645ba8d576d6e48727f772650a349a85e14
Adam Fluke <fluke.adam@gmail.com>
2015-11-24 22:27:33 -0800

    Add feature example for going to login page

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
-feature "Going to the homepage", js: false do
+feature "Visiting Public Not Protected Pages", js: false do
   scenario "Visiting the homepage" do
     visit root_path
     expect(page).to have_content 'BipHub'
     expect(page).to have_content 'Login'
+  end
+  scenario "Going to the login page" do
+    visit root_path
+    click_on 'Login'
+    expect(page).to have_content 'Email:'
+    expect(page).to have_content 'Password:'
+    expect(page).to have_selector 'form'
   end
 end
