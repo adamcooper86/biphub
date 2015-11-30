3ee247c9d4623e64cd5df82bccc8a797caa0a469
Adam Fluke <fluke.adam@gmail.com>
2015-11-30 12:41:33 -0800

    Add first test to school crud tests

--- spec/features/admin_schools_crud_spec.rb
+++ spec/features/admin_schools_crud_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
-feature "Administrator Crud for Schools", js: false do
+feature "Administrator Crud for Schools", js: true do
   given(:school){ School.create }
 
+
   background do
+    Admin.create email: 'AdminUser@biphub.com', password: 'Password', password_confirmation: 'Password'
     visit login_path
     within "#login_form" do
       fill_in 'email', with: "AdminUser@biphub.com"
       fill_in 'password', with: "Password"
     end
     click_on 'Submit'
+  end
+
+  scenario 'Creating a new school' do
+    click_on 'New School'
+    expect(page).to have_selector "#new_school"
   end
 end
