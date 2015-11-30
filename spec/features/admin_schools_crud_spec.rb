ae7784dfb4ea1f02fbb2bddcc7711c34a1d230cc
Adam Fluke <fluke.adam@gmail.com>
2015-11-30 13:08:54 -0800

    Create update/read feature test for schools

--- spec/features/admin_schools_crud_spec.rb
+++ spec/features/admin_schools_crud_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-feature "Administrator Crud for Schools", js: true do
-  given(:school){ School.create }
-
-
+feature "Administrator Crud for Schools", js: false do
   background do
     Admin.create email: 'AdminUser@biphub.com', password: 'Password', password_confirmation: 'Password'
+    School.create name: 'TestSchool', address: 'TestAddress', city: 'TestCity', state: 'ST', zip: '00000'
+
     visit login_path
     within "#login_form" do
       fill_in 'email', with: "AdminUser@biphub.com"
@@ -17,5 +16,41 @@ feature "Administrator Crud for Schools", js: true do
   scenario 'Creating a new school' do
     click_on 'New School'
     expect(page).to have_selector "#new_school"
+
+    within "#new_school" do
+      fill_in 'school_name', with: "School-Name-Test"
+      fill_in 'school_address', with: "Address"
+      fill_in 'school_city', with: "City"
+      fill_in 'school_state', with: "ST"
+      fill_in 'school_zip', with: "00000"
+    end
+    click_on 'Create School'
+
+    expect(page).to have_content "School-Name-Test"
+  end
+  scenario 'Viewing the school show page' do
+    within '#schoolsPanel' do
+      click_on 'show'
+    end
+    expect(page).to have_selector "#schoolInformation"
+    expect(page).to have_content "TestSchool"
+  end
+  scenario 'Editing the school information' do
+    within '#schoolsPanel' do
+      click_on 'edit'
+    end
+    expect(page).to have_selector '.edit_school'
+
+    within '.edit_school' do
+      fill_in 'school_name', with: "School-Name-Test"
+      fill_in 'school_address', with: "Address"
+      fill_in 'school_city', with: "City"
+      fill_in 'school_state', with: "ST"
+      fill_in 'school_zip', with: "00000"
+    end
+    click_on 'Update School'
+
+    expect(page).to have_selector "#schoolInformation"
+    expect(page).to have_content "School-Name-Test"
   end
 end
