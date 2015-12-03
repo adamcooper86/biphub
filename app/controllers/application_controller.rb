f268396cd1393eb9402214bdbeecbf1f45624f24
Adam Fluke <fluke.adam@gmail.com>
2015-12-02 19:03:55 -0800

    Add rescue for invalide user id in session data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      session.delete(:user_id)
+      nil
   end
   helper_method :current_user
 
