3406daba48c6f065cef84ee9bb6a61edff00430f
Adam Fluke <fluke.adam@gmail.com>
2015-12-03 18:43:33 -0800

    Create the begining framework for the observationspanel

--- spec/features/teacher_actions_spec.rb
+++ spec/features/teacher_actions_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 feature "Using teacher dashboard", js: false  do
+  given(:teacher){ FactoryGirl.create(:teacher) }
+  given(:observation){ FactoryGirl.create(:observation, user: teacher) }
   background do
-    school = School.create name: 'TestSchool', address: 'TestAddress', city: 'TestCity', state: 'ST', zip: '00000'
-    teacher = Teacher.create first_name: 'TestTea', last_name: 'Testcher', email: 'teacher@biphub.com', password: 'Password', password_confirmation: 'Password'
-    school.teachers << teacher
-
     visit login_path
     within "#login_form" do
-      fill_in 'email', with: "teacher@biphub.com"
-      fill_in 'password', with: "Password"
+      fill_in 'email', with: teacher.email
+      fill_in 'password', with: teacher.password
     end
     click_on 'Submit'
   end
@@ -24,6 +22,26 @@ feature "Using teacher dashboard", js: false  do
   feature "Teacher panel contents" do
     scenario 'there is a confirmation message that the card queue is empty' do
       expect(page).to have_content "Your queue is empty. Good job!!"
+    end
+  end
+
+  feature "Can Use the ObservationsPanel" do
+    scenario 'by seeing a message if there are no observations'  do
+      expect(page).to have_selector "#observationsTable"
+      expect(page).to have_content "We didn't find any answered observations"
+    end
+    scenario 'by seeing the observations they have answered'  do
+      observation
+      click_on 'Dashboard'
+      expect(page).to have_selector "#observationsTable"
+      expect(page).to have_content teacher.last_name
+    end
+    scenario 'but not seeing the observations they have not answered'  do
+      FactoryGirl.create(:record, observation: observation)
+      click_on 'Dashboard'
+      expect(page).to have_selector "#observationsTable"
+      expect(page).not_to have_selector ".observationRow"
+      expect(page).to have_content "We didn't find any answered observations"
     end
   end
 end
