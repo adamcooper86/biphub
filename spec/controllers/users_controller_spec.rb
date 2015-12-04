7349e87f54d40f19e24aab23919f95dba5cc8284
Adam Fluke <fluke.adam@gmail.com>
2015-12-04 11:41:24 -0800

    Refactor users controller to remove school creation

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,15 +1,13 @@
 require "rails_helper"
 
 RSpec.describe UsersController, :type => :controller do
-  let(:school){ FactoryGirl.create :school }
   let(:user){ FactoryGirl.create :teacher}
   before(:each){
     allow(controller).to receive(:current_user).and_return(user)
-    school.teachers << user
   }
 
   context "Get #show" do
-    subject{ get :show, school_id: school.id, id: user.id }
+    subject{ get :show, school_id: user.school.id, id: user.id }
     it "responds successfully with an HTTP 200 status code" do
       expect(controller).to receive(:authorize)
       is_expected.to have_http_status(200)
