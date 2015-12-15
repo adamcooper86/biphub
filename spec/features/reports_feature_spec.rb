688c55360435c8c3c0541f62b0a82f727f9ce3ef
Andrew Patterson <andrew.patterson.3001@gmail.com>
2015-12-15 14:33:35 -0800

    write feature test to check that selected student data appears in report table

--- spec/features/reports_feature_spec.rb
+++ spec/features/reports_feature_spec.rb
@@ -8,19 +8,20 @@ feature "Using the reports panel", js: false, focus: true do
   given(:student){ Student.create first_name: 'TestStudent', last_name: 'TestLastName' }
   given(:card){ Card.create start: Time.now, finish: Time.now  }
   given(:bip){ Bip.create start: Time.now, end: Time.now  }
-  given(:goal){ Goal.create prompt: "Prompt", text: "Text", meme: "Time"  }
-
+  given(:goal){ Goal.create prompt: "Goal 1 Prompt", text: "Goal 1 Text", meme: "Time"  }
+  given(:observation){ Observation.create student_id: student.id, user_id: teacher.id, start:Time.now, finish:Time.now}
+  given(:record){ Record.create observation_id: observation.id, result: 7}
   background do
     school.coordinators << coordinator
     school.students << student
     school.teachers << teacher
     school.speducators << speducator
-    speducator.students << student
+    speducator.case_students << student
     student.cards << card
     teacher.cards << card
     student.bips << bip
     bip.goals << goal
-
+    record
     visit login_path
     within "#login_form" do
       fill_in 'email', with: speducator.email
@@ -29,16 +30,34 @@ feature "Using the reports panel", js: false, focus: true do
     click_on 'Submit'
   end
 
-  scenario ' by going to the reports dashboard' do
+  scenario 'by going to the reports dashboard' do
     click_on 'Reports'
-
     expect(page).to have_selector '#reportsPanel'
-
     expect(page).to have_selector '#studentFilter'
     expect(page).to have_content speducator.last_name
     expect(page).to have_content "No student selected."
     expect(page).not_to have_selector ".graph"
     expect(page).not_to have_selector ".studentData"
+  end
+
+  scenario 'by selecting a student to view' do
+    click_on 'Reports'
+    #within the form select a student and submit the form
+    within '#studentFilter' do
+      select student.first_name, :from => 'student_id'
+    end
+    click_on 'Filter Results'
+    expect(page).to have_selector '#reportsPanel'
+    expect(page).to have_selector '#studentFilter'
+    expect(page).to have_selector ".graph"
+    expect(page).to have_selector ".studentData"
+
+    expect(page).not_to have_content "No student selected."
+    expect(page).to have_content speducator.last_name
+
+    expect(page).to have_content student.last_name
+    expect(page).to have_content goal.prompt
+    expect(page).to have_content record.result
 
   end
 end
