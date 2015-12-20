e9278eea4a6c97e477aa0dd5be1f1b0f2bbb2b9f
Adam Fluke <fluke.adam@gmail.com>
2015-12-19 18:53:55 -0800

    Create avg_perfomance for qualitative records

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,4 +3,10 @@ class Goal < ActiveRecord::Base
   has_many :records
 
   validates :bip, :text, :prompt, :meme, presence: true
+
+  def avg_performance
+    results = self.records.map{ |record| record.result }
+    average_result = results.inject(0.0) { |sum, el| sum + el } / results.size
+    (average_result / 5) *100
+  end
 end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,9 +1,56 @@
 require 'rails_helper'
 
-RSpec.describe Goal, type: :model do
+RSpec.describe Goal, type: :model, focus: true do
   context 'validations' do
     it 'is invalid with no bip' do
       expect(FactoryGirl.build(:goal, bip: nil)).not_to be_valid
+    end
+  end
+  describe '#avg_performance' do
+    context 'qualitative goals' do
+      let(:goal){ FactoryGirl.create :goal, meme: "Qualitative" }
+      let(:record){ FactoryGirl.create :record, goal: goal}
+
+      it 'returns 100.00 when results are 5/5' do
+        record.result = 5
+        record.save
+
+        expect(goal.avg_performance).to eq 100.0
+      end
+      it 'returns 80.00 when results are 4/5' do
+        record.result = 4
+        record.save
+
+        expect(goal.avg_performance).to eq 80.0
+      end
+      it 'returns 60.00 when results are 3/5' do
+        record.result = 3
+        record.save
+
+        expect(goal.avg_performance).to eq 60.0
+      end
+      it 'returns 40.00 when results are 2/5' do
+        record.result = 2
+        record.save
+
+        expect(goal.avg_performance).to eq 40.0
+      end
+      it 'returns 20.00 when results are 1/5' do
+        record.result = 1
+        record.save
+
+        expect(goal.avg_performance).to eq 20.0
+      end
+      it 'returns 0.00 when results are 1/5' do
+        record.result = 0
+        record.save
+
+        expect(goal.avg_performance).to eq 0.0
+      end
+      it 'averages multiple records' do
+        record1, record2, record3 = FactoryGirl.create(:record, goal: goal, result: 5), FactoryGirl.create(:record, goal: goal, result: 4), FactoryGirl.create(:record, goal: goal, result: 2)
+        expect(goal.avg_performance).to eq 73.33333333333333
+      end
     end
   end
 end
