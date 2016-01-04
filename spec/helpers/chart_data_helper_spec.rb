03a7c69bfa5d873966bbe6f46ba989df3a9f6e3d
Adam Fluke <fluke.adam@gmail.com>
2016-01-04 10:55:27 -0800

    Add slice filter options to chart data helper method, school_data

--- spec/helpers/chart_data_helper_spec.rb
+++ spec/helpers/chart_data_helper_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.describe ChartDataHelper, type: :helper, focus: false do
   let(:school){ FactoryGirl.create(:school) }
-  let(:student){ FactoryGirl.create(:student, school: school) }
+  let(:speducator){ FactoryGirl.create(:speducator, school: school) }
+  let(:student){ FactoryGirl.create(:student, school: school, grade: 1, gender: "female", race: "Asian") }
+  let(:student2){ FactoryGirl.create(:student, school: school, grade: 2, gender: "male", race: "White", speducator: speducator) }
   let(:bip){ FactoryGirl.create :bip, student: student }
+  let(:bip2){ FactoryGirl.create :bip, student: student2 }
   let(:goal){ FactoryGirl.create :goal, bip: bip, meme: "Qualitative" }
+  let(:goal2){ FactoryGirl.create :goal, bip: bip2, meme: "Qualitative" }
   let(:observation1){ FactoryGirl.create(:observation, student: student, start: Time.now - 1.day, finish: Time.now - 3.days) }
   let(:record1){ FactoryGirl.create(:record, observation: observation1, goal: goal, result: 5) }
-  let(:observation2){ FactoryGirl.create(:observation, student: student, start: Time.now - 1.day, finish: Time.now - 2.days) }
-  let(:record2){ FactoryGirl.create(:record, observation: observation2) }
-  let(:observation3){ FactoryGirl.create(:observation, student: student, start: Time.now - 1.day, finish: Time.now - 1.day) }
-  let(:record3){ FactoryGirl.create(:record, observation: observation3) }
+  let(:observation2){ FactoryGirl.create(:observation, student: student2, start: Time.now - 1.day, finish: Time.now - 2.days) }
+  let(:record2){ FactoryGirl.create(:record, observation: observation2, goal: goal2, result: 4) }
 
-  context "#school_data" do
+  describe "#school_data" do
     it "returns an empty array when there are results" do
       expect(helper.school_data(school)).to eq([['Day', 'Performance']])
     end
@@ -20,6 +22,16 @@ RSpec.describe ChartDataHelper, type: :helper, focus: false do
       record1
       finish_date = observation1.finish.to_date
       expect(helper.school_data(school)).to eq([['Day', 'Performance'],[finish_date, 100.0]])
+    end
+    context 'It accepts an optional filter of slicers' do
+      let(:filter){ {grade: 2, gender: "male", race: "White", speducator_id: speducator.id} }
+
+      it 'returns only the filtered data pairs' do
+        record1
+        record2
+        finish_date = observation2.finish.to_date
+        expect(helper.school_data(school, filter)).to eq([['Day', 'Performance'],[finish_date, 80.0]])
+      end
     end
   end
 end
