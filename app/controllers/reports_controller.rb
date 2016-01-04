e414256ec5e9dac2b97614c12b67115d895bdef7
Adam Fluke <fluke.adam@gmail.com>
2016-01-04 11:15:04 -0800

    Refactor reports#index to make filter in seprate method call

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,19 +8,8 @@ class ReportsController < ApplicationController
         @school = School.find(params[:school_id])
       end
 
-      @grade = params[:grade_lvl]
-      @grade = nil if @grade == ""
+      make_filter
 
-      @gender = params[:gender]
-      @gender = nil if @gender == ""
-
-      @race = params[:race]
-      @race = nil if @race == ""
-
-      @speducator_id = params[:speducator_id]
-      @speducator_id = nil if @speducator_id == ""
-
-      @filter = { grade: @grade, gender: @gender, race: @race, speducator_id: @speducator_id }
       render "admin_index"
     elsif @user
       @students = @user.case_students
@@ -31,5 +20,23 @@ class ReportsController < ApplicationController
     else
       redirect_to login_path
     end
+  end
+
+  private
+
+  def make_filter
+    @grade = params[:grade_lvl]
+    @grade = nil if @grade == ""
+
+    @gender = params[:gender]
+    @gender = nil if @gender == ""
+
+    @race = params[:race]
+    @race = nil if @race == ""
+
+    @speducator_id = params[:speducator_id]
+    @speducator_id = nil if @speducator_id == ""
+
+    @filter = { grade: @grade, gender: @gender, race: @race, speducator_id: @speducator_id }
   end
 end
